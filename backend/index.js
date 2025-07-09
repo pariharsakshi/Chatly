4d1df9a362355d1e550490abb1a702c62dbbaaeb
SAKSHI PARIHAR <130368553+pariharsakshi@users.noreply.github.com>
2025-07-09 12:53:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://realtimechatapp-dz8u.onrender.com",
     credentials:true
 }))
 app.use(express.json())
